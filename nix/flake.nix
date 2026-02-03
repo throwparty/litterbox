de00df15d7ada911a9213b439d7056231ea45d1b
Luke Carrier <luke@carrier.family>
2026-02-03 21:56:54 +0000

    nix: starter flake

---
+++ nix/flake.nix
@@ -0,0 +1,60 @@
+{
+  description = "throwparty/litterbox";
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/release-25.11";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    throwparty = {
+      url = "git+ssh://git@github.com/throwparty/nix";
+      inputs.flake-utils.follows = "flake-utils";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  outputs =
+    { flake-utils, nixpkgs, rust-overlay, throwparty, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            (import rust-overlay)
+          ];
+        };
+        rustToolchain = pkgs.rust-bin.stable.latest.default.override {
+          targets = [ "wasm32-wasip1" ];
+        };
+        inherit (pkgs.lib) getExe getExe';
+      in
+      {
+        devShells.default =
+          let
+            inherit (throwparty.devShells.${system}) commonTools;
+            inherit (pkgs)
+              cargo-auditable
+              python3
+              ;
+            toolVersions = throwparty.lib.mkToolVersions {
+              inherit pkgs;
+              name = "default";
+              commands = ''
+                ${getExe' rustToolchain "cargo"} --version
+                ${getExe cargo-auditable} --version
+                ${getExe python3} --version
+                ${getExe' rustToolchain "rustc"} --version
+              '';
+            };
+          in
+          pkgs.mkShell {
+            buildInputs = [
+              cargo-auditable
+              python3
+              rustToolchain
+            ];
+            shellHook = "\n cat ${toolVersions}";
+          };
+      }
+    );
+}
